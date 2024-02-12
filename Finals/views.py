35d5ad4c447cd88ad406a6d42ae31b5f66fb6b5c
John Gachara <johngachara29@gmail.com>
2024-02-12 08:49:17 +0300

    device binding in progress

--- Finals/views.py
+++ Finals/views.py
@@ -1,5 +1,4 @@
 import random
-
 from django.contrib.auth import login
 from django.contrib.auth.models import User
 from django.core.mail import send_mail
@@ -31,13 +30,13 @@ def signup(request):
                 form.add_error('email', 'Please use your individual school email address.')
                 return render(request, 'signup.html', {"Form": form})
 
-            # Activate the user and save
-            new_user.is_active = True
+            if User.objects.filter(email=user_data['email']).exists():
+                form.add_error('email', 'This email is already in use')
+                return render(request, 'signup.html', {"Form": form})
+
             new_user.save()
-
             # Optionally, you can proceed with email verification here
-
-            return redirect('home')
+            return redirect('verif',new_user.id)
 
     else:
         form = Signup()
@@ -47,9 +46,42 @@ def signup(request):
 
 def email_verification(request,user_id):
     user = User.objects.get(pk=user_id)
-    return HttpResponse(f"{user} found")
+    return render(request,'email.html',{"User":user})
 
 
+def verify_email(request,user_id):
+    user = User.objects.get(pk=user_id)
+    sender_email = settings.EMAIL_HOST_USER
+    heading = "Your verification code"
+    recipient_email = user.email
+    verification_code = str(random.randint(100000, 999999))
+    request.session['verification_code'] = verification_code
+    email_content = f"Your Verification code is {verification_code}"
+    mail = send_mail(heading,email_content, sender_email, [recipient_email])
+    return redirect('code',user.id)
+
+def verify_code(request,user_id):
+    user = User.objects.get(pk=user_id)
+    stored_verification_code = request.session.get('verification_code')
+    if request.method == 'POST':
+        submitted_code = request.POST.get('verification_code')
+        if submitted_code == stored_verification_code:
+            # Code is correct, perform necessary actions (e.g., activate the user)
+            user.is_active = True
+            user.save()
+
+
+            # Clear the verification code from the session
+            del request.session['verification_code']
+            return redirect('home')
+        else:
+            return render(request, 'verify.html', {"User_id": user.id})
+
+    return render(request,'verify.html',{"User_id":user.id})
+
+
+def bind_device(request):
+    return render(request,'bind.html')
 def take_photo(request):
     return render(request,'photo.html')
 
