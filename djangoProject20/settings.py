d5fb46c1e1b5e788be9d0ecc780d899c3dcc08d1
John Gachara <johngachara29@gmail.com>
2024-03-07 05:21:01 +0300

    progress

--- djangoProject20/settings.py
+++ djangoProject20/settings.py
@@ -82,6 +82,19 @@ WSGI_APPLICATION = 'djangoProject20.wsgi.application'
 
 
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'j39342336i',
+        'HOST': 'postgresql-rds-production-wordy-papaya-gvln.ca5z9zbxudjn.us-west-2.rds.amazonaws.com',  # Set to the appropriate host
+        'PORT': '5432',       # Set to the appropriate port
+    }
+}
+
+
+
 
 
 
